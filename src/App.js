c4140e0dbe7a20ae0f20b75b1f133c9d1b55d1ce
Chulindra Rai <87846923+IotchulindraRai@users.noreply.github.com>
2023-08-11 23:00:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+const projectID = '7a060015-8879-4a12-a66a-aa9def783bb2';
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+//Chulindra
+export default App;
